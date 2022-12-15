64b6af6e75f0bf80c1deab8af1e867e2129a726b
Shane Celis <shane.celis@gmail.com>
2022-12-15 03:05:17 -0500

    chore: Initial import.

---
+++ befunge-game.js
@@ -0,0 +1,172 @@
+/*
+@title: scrawny_koffing
+@author: your_name
+*/
+
+const player = "p";
+const selection = "b";
+
+class Befunge {
+
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.cells = Array(width).fill(null).map(i => Array(height).fill(' '));
+    this.pointer = [0, 0];
+    this.inertia = [1, 0];
+    this.stack = [];
+    this.output = "";
+    this.stringMode = false;
+  }
+
+  isDigit(c) {
+    return c >= '0' && c <= '9';
+  }
+
+  eval(instruction) {
+    let s = this.stack;
+    if (this.stringMode) {
+      if (instruction == '"')
+        this.stringMode = false;
+      else
+        s.push(instruction);
+      return;
+    }
+    if (this.isDigit(instruction)) {
+      s.push(instruction);
+      return;
+    }
+    switch(instruction) {
+      case ' ':
+        break;
+      case '+':
+        s.push(s.pop() + s.pop());
+        break;
+      case '-':
+        s.push(s.pop() - s.pop());
+        break;
+      case '.':
+        // Output as integer.
+        //let e = s.pop();
+        this.output += s.pop();
+        break;
+      case ',':
+        // Output as character.
+        this.output += s.pop();
+        break;
+      case '"':
+        this.stringMode = true;
+        break;
+      default:
+        // This code is probably invalid. Ignoring.
+    }
+  }
+
+  step(count) {
+    let pc = this.pointer;
+    for (let i = 0; i < count; i++) {
+      let instruction = this.cells[pc[0]][pc[1]];
+      this.eval(instruction);
+      pc[0] += this.inertia[0];
+      pc[1] += this.inertia[1];
+    }
+  }
+
+  read(input) {
+    for (let i = 0, j = 0, k = 0; k < input.length; k++) {
+      let c = input.charAt(k);
+      if (c == '\n') {
+        j++;
+        i = 0;
+        continue;
+      }
+      if (i >= this.width) {
+        j++;
+        i = 0;
+      }
+      this.cells[i][j] = c;
+      i++;
+    }
+  }
+
+  draw(x, y) {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        addText(this.cells[i][j], { x: i + x, y: j + y});
+      }
+    }
+  }
+}
+
+setLegend(
+  [ player, bitmap`
+................
+................
+.......000......
+.......0.0......
+......0..0......
+......0...0.0...
+....0003.30.0...
+....0.0...000...
+....0.05550.....
+......0...0.....
+.....0....0.....
+.....0...0......
+......000.......
+......0.0.......
+.....00.00......
+................`],
+   [ selection, bitmap`
+6666666666666666
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6..............6
+6666666666666666`]
+);
+
+setSolids([]);
+
+let level = 0;
+const levels = [
+  map`
+pb.
+...
+...`,
+];
+
+let befunge = new Befunge(10, 10);
+befunge.read("11+.\"a\". elllkjlkj@");
+befunge.step(8);
+
+addText("output: " + befunge.output, { x: 0, y: 15 });
+
+
+//befunge.draw(5, 5);
+
+setMap(levels[level]);
+
+setPushables({
+  [player]: []
+});
+
+//addText("hello", { x: 0, y: 0, color: color`5` });
+//addText("hello", { x: 18, y: 5, color: color`5` });
+
+onInput("s", () => {
+  getFirst(selection).y += 1
+});
+
+afterInput(() => {
+
+});
